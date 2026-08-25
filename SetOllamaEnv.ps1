cff5904c3591e1fc4cb86141f08c03aa6f35e9fc
Chris Lesemann <clesemann@gmail.com>
2026-08-25 07:32:11 -0700

    Updated .env and SetOllamaEnv.ps1

--- SetOllamaEnv.ps1
+++ SetOllamaEnv.ps1
@@ -1,3 +1,3 @@
-[Environment]::SetEnvironmentVariable("OLLAMA_MODELS", "C:\ollama\models", "User")
+[Environment]::SetEnvironmentVariable("OLLAMA_MODELS", "C:\projects\models", "User")
 [Environment]::SetEnvironmentVariable("CUDA_VISIBLE_DEVICES", "1", "User")
 [Environment]::SetEnvironmentVariable("OLLAMA_HOST", "0.0.0.0:11434", "User")
